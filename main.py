a5cfcb45007bdf4898241dba26947c8406e83fcf
aiaidada <aidaebrahimi56@gmail.com>
2023-05-20 18:16:14 -0400

    First Test

--- main.py
+++ main.py
@@ -7,8 +7,10 @@ import os
 import torch
 import numpy as np
 import math
+from pathlib import Path
 
-from src.diffusers.pipelines import EstimDiffPipeline
+
+from src.diffusers.pipelines import EstimDiffPipeline , DDIMPipeline
 
 
 torch.set_printoptions(sci_mode=False)
@@ -47,7 +49,7 @@ def parse_args_and_config():
     
     parser.add_argument("--Dataset" , default= "google/ddpm-church-256" , type= str, help= "Dataset for generated photo")
 
-    parser.add_argument() 
+    #parser.add_argument() 
     args = parser.parse_args()
 
 
@@ -72,6 +74,11 @@ def main():
     ddim = ddim.to(device)
     im_num = args.Generation_num
     step_num = args.Step_num
+    if not(args.Directory == None):
+        path = Path(args.Directory)
+    else:
+        path = Path('Samples')
+    path.mkdir(exist_ok=True)
     if args.skipping == 'Uniform':
         speed_up = 1000/step_num
         skip_num = math.ceil(2 * speed_up) - 2
@@ -80,10 +87,31 @@ def main():
     else:
         uniform = False
         skip_num = 5
-    for i in range (im_num):
-        image = torch.randn((1, 3 , 256 , 256) , device= device)
-        im = ddim(num_inference_steps= 1000 , case_num= 47 , image= image , threshold= args.Skip_threshold , skip_num= skip_num , uniform= uniform , final= final)
-        im['images'][0].save("Sample_" + str(i) + ".png") 
+    if args.Mode == 'Normal':
+        for i in range (im_num):
+            image = torch.randn((1, 3 , 256 , 256) , device= device)
+            im = ddim(num_inference_steps= 1000 , case_num= 47 , image= image , threshold= args.Skip_threshold , skip_num= skip_num , uniform= uniform , final= final)
+            im['images'][0].save(os.path.join(str(path), "Sample_" + str(i) + ".png"))
+    elif args.Mode == 'Test':
+        original_path = Path(str(path) + '\DDIM(1000)')
+        original_path.mkdir(exist_ok= True)
+        DDIM_path = Path(str(path) + '\DDIM('+str(step_num) + ')')
+        DDIM_path.mkdir(exist_ok= True)
+        Proposed_path = Path(str(path) + '\Proposed('+str(step_num) + ')')
+        Proposed_path.mkdir(exist_ok= True)
+        for i in range (im_num):
+            image = torch.randn((1, 3 , 256 , 256) , device= device)
+            im = ddim(num_inference_steps= 1000 , case_num= 47 , image= image , threshold= args.Skip_threshold , skip_num= skip_num , uniform= uniform , final= final)
+            im['images'][0].save(os.path.join(str(Proposed_path), "Sample0_" + str(i) + ".png"))
+            im = ddim(num_inference_steps= 1000 , case_num= 0 , image= image , threshold= args.Skip_threshold , skip_num= skip_num , uniform= uniform , final= final)
+            im['images'][0].save(os.path.join(str(original_path), "Sample_" + str(i) + ".png"))
+            im = ddim(num_inference_steps= step_num , case_num= 0 , image= image , threshold= args.Skip_threshold , skip_num= skip_num , uniform= uniform , final= final)
+            im['images'][0].save(os.path.join(str(DDIM_path), "Sample_" + str(i) + ".png"))
+    else:
+        print('Argument should only be Test or Noraml')
+
+
+        
 
     return 0
 

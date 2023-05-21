c41b1f38bce0f72246289b3d27c7e5108bf27f81
aiaidada <aidaebrahimi56@gmail.com>
2023-05-21 02:35:40 -0400

    Test_90_comp

--- main.py
+++ main.py
@@ -7,7 +7,10 @@ import os
 import torch
 import numpy as np
 import math
+import cv2
 from pathlib import Path
+from pytorch_fid import fid_score
+import yaml
 
 
 from src.diffusers.pipelines import EstimDiffPipeline , DDIMPipeline
@@ -15,6 +18,13 @@ from src.diffusers.pipelines import EstimDiffPipeline , DDIMPipeline
 
 torch.set_printoptions(sci_mode=False)
 
+def PSNR(original, compressed):
+    mse = np.mean((original - compressed) ** 2)
+    if(mse == 0):
+        return 100
+    max_pixel = 255.0
+    psnr = 20 * log10(max_pixel / sqrt(mse))
+    return psnr
 
 def parse_args_and_config():
     parser = argparse.ArgumentParser(description=globals()["__doc__"])
@@ -93,12 +103,14 @@ def main():
             im = ddim(num_inference_steps= 1000 , case_num= 47 , image= image , threshold= args.Skip_threshold , skip_num= skip_num , uniform= uniform , final= final)
             im['images'][0].save(os.path.join(str(path), "Sample_" + str(i) + ".png"))
     elif args.Mode == 'Test':
-        original_path = Path(os.join(str(path) , 'DDIM(1000)'))
+        original_path = Path(os.path.join(str(path) , 'DDIM(1000)'))
         original_path.mkdir(exist_ok= True)
-        DDIM_path = Path(os.path(str(path) , '\DDIM('+str(step_num) + ')'))
+        DDIM_path = Path(os.path.join(str(path) , '\DDIM('+str(step_num) + ')'))
         DDIM_path.mkdir(exist_ok= True)
-        Proposed_path = Path(os.path.join(str(path) + '\Proposed('+str(step_num) + ')'))
+        Proposed_path = Path(os.path.join(str(path) ,'\Proposed('+str(step_num) + ')'))
         Proposed_path.mkdir(exist_ok= True)
+        proposed_PSNR = 0
+        ddim_PSNR = 0
         for i in range (im_num):
             image = torch.randn((1, 3 , 256 , 256) , device= device)
             im = ddim(num_inference_steps= 1000 , case_num= 47 , image= image , threshold= args.Skip_threshold , skip_num= skip_num , uniform= uniform , final= final)
@@ -107,6 +119,22 @@ def main():
             im['images'][0].save(os.path.join(str(original_path), "Sample_" + str(i) + ".png"))
             im = ddim(num_inference_steps= step_num , case_num= 0 , image= image , threshold= args.Skip_threshold , skip_num= skip_num , uniform= uniform , final= final)
             im['images'][0].save(os.path.join(str(DDIM_path), "Sample_" + str(i) + ".png"))
+            original = cv2.imread(os.path.join(str(original_path), "Sample_" + str(i) + ".png"))
+            compressed = cv2.imread(os.path.join(str(DDIM_path), "Sample_" + str(i) + ".png"), 1)
+            ddim_PSNR += PSNR(original, compressed)
+            compressed = cv2.imread(os.path.join(str(Proposed_path), "Sample_" + str(i) + ".png"), 1)
+            proposed_PSNR += PSNR(original , compressed)         
+        proposed_PSNR /= im_num
+        ddim_PSNR /= im_num
+        proposed_FID =  fid_score.calculate_fid_given_paths([str(original_path) , str(Proposed_path)] , device= device , batch_size= 1 , dims= 2048)
+        previous_FID =  fid_score.calculate_fid_given_paths([str(original_path) , str(DDIM_path)] , device= device , batch_size= 1  , dims= 2048)
+        data  ={ 'DDIM_PSNR': ddim_PSNR ,
+                'DDIM_FID' : previous_FID,
+                'EstimDiff_PSNR' : proposed_PSNR,
+                'EstimDiff_FID' : proposed_FID
+        }
+        with open('output.yaml', 'w') as file:
+            yaml.dump(data, file)
     else:
         raise TypeError('Argument should only be Test or Noraml')
 
